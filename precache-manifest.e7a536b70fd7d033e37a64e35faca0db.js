83c593fb87a243a632486076619c319f192cf48e
MelyHC <mely.hidalgo.crespo@gmail.com>
2018-11-08 15:40:10 -0500

    Updates

---
+++ precache-manifest.e7a536b70fd7d033e37a64e35faca0db.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7f39bde8709669fc9e9577665f41b7a",
+    "url": "/Belcorp-Hackaton-2018/static/media/lbelLogo.a7f39bde.PNG"
+  },
+  {
+    "revision": "795396cc236505ac62721e4db6491931",
+    "url": "/Belcorp-Hackaton-2018/static/media/esikaLogo.795396cc.PNG"
+  },
+  {
+    "revision": "6421205e809312afae93aede8a5ac72d",
+    "url": "/Belcorp-Hackaton-2018/static/media/cyzoneLogo.6421205e.PNG"
+  },
+  {
+    "revision": "1b19ea765c186a56ffceb76d52428d81",
+    "url": "/Belcorp-Hackaton-2018/static/media/belcorp.1b19ea76.png"
+  },
+  {
+    "revision": "b4d30698d38dfd6872e22eb35bf45252",
+    "url": "/Belcorp-Hackaton-2018/static/media/Captura.b4d30698.JPG"
+  },
+  {
+    "revision": "081d556f831a83c7cd3e",
+    "url": "/Belcorp-Hackaton-2018/static/js/runtime~main.081d556f.js"
+  },
+  {
+    "revision": "11e8b3ad2da2c98395e8",
+    "url": "/Belcorp-Hackaton-2018/static/js/main.11e8b3ad.chunk.js"
+  },
+  {
+    "revision": "d35ff613940aab71f06d",
+    "url": "/Belcorp-Hackaton-2018/static/js/1.d35ff613.chunk.js"
+  },
+  {
+    "revision": "11e8b3ad2da2c98395e8",
+    "url": "/Belcorp-Hackaton-2018/static/css/main.687176a4.chunk.css"
+  },
+  {
+    "revision": "77ea25f9de9d20fc480759b256ca39fc",
+    "url": "/Belcorp-Hackaton-2018/index.html"
+  }
+];
